796f862029cd4314a0ccaadf41773bdd05c2a61c
Arina Vityazeva <A.Vityazeva@compassplus.com>
2021-02-11 22:58:52 +0500

    Add lesson2

---
+++ Lesson2-3/scriptJS.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let userFirstName = "Arina";
+let userLasName = "Vityazeva";
+
+let userBirthDay = prompt("Enter Birth Day:", "");
+let userBirthMonth = prompt("Enter Birth Month:", "");
+let userBirthYear = prompt("Enter Birth Year:", "");
+
+let userAge = 2020-userBirthYear;
+alert(userFirstName+" "+userLasName+" "+userBirthDay+"/"+userBirthMonth+"/"+userBirthYear+" "+userAge+" years old") ;
